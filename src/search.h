bfd64350fee145b1420d528a10d6f301c70570bb
izma <izmamustafa34601@gmail.com>
2026-01-03 00:00:41 +0500

    define the search function in search module

---
+++ src/search.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Function declaration
+void search(const string &filename, const string &index);
+
+#endif
