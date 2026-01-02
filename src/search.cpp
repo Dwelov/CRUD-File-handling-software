5a04b8bd2629a5191f68e30a16fd34e6af07728e
izma <izmamustafa34601@gmail.com>
2026-01-03 00:26:32 +0500

    Define the search function

---
+++ src/search.cpp
@@ -0,0 +1,25 @@
+#include "search.h"
+
+void search(const string &filename,const string& index) {
+    ifstream file(filename);
+    if(!file){
+        cerr<<"Error opening the file"<<endl;
+        return;
+    }
+    
+    string line;
+    bool found = false;
+
+    while (getline(file, line)) {
+        if (line.find(index) != string::npos) {
+            cout << "Found: " << line << endl;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "Index not found." << endl;
+    }
+
+    file.close();
+}
