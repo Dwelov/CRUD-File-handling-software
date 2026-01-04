cb20b144936f611b2b259f0f8a7cfbacbcdeb215
Dwelov <sirajmeher984@gmail.com>
2026-01-04 13:51:23 +0500

    added function prototypes to add, upadet and delete the file data

---
+++ src/crud.h
@@ -0,0 +1,26 @@
+#ifndef CRUD_CPP
+#define CRUD_CPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+
+//daniyal add your  functions  here
+
+// function to add students in the file
+void addStudents(const string &filename, const string &rollNumber, const string &newName,
+                 const vector<int> &newMarks, float newAverage);
+
+// function to update the file
+void updateRecord(const string &filename, const string &rollNumber, const string &newName,
+                  const vector<int> &newMarks, float newAverage);
+
+// function to delete the file
+
+void deleteRecord(const string &filename, const string &rollNumber);
+
+#endif
